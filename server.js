483f18cdc52f0d04b1768285c26ba388c744dcd9
Darragh Ferry <darraghferry@hotmail.com>
2020-08-05 21:19:34 +0100

    added peerjs

--- server.js
+++ server.js
@@ -11,8 +11,15 @@ app.get("/", (req, res) => {
   res.redirect(`/${uuidV4()}`);
 });
 
-app.get("/room", (req, res) => {
+app.get("/:room", (req, res) => {
   res.render("room", { roomId: req.params.room });
+});
+
+io.on("connection", socket => {
+  socket.on("join-room", (roomId, userId) => {
+    socket.join(roomId);
+    socket.to(roomId).broadcast.emit("user-connected", userId);
+  });
 });
 
 server.listen(3000);
